78dc7c6a9ab3bf58ea4bb5f77e54f4e9a9b37a49
mithhu <mahamudul.hasan@evaly.com.bd>
2021-02-16 03:07:41 +0600

    core features of youtube

---
+++ client/build/precache-manifest.ca2934d69d8f418ff44c33f2721f83c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33eaba0b64a72f28ca03a99e02ac55a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a49c593e07a8c21b75c9",
+    "url": "/static/css/2.4858d7ec.chunk.css"
+  },
+  {
+    "revision": "a49c593e07a8c21b75c9",
+    "url": "/static/js/2.4708429a.chunk.js"
+  },
+  {
+    "revision": "d406cdd2ff777bb1780875f438877cc2",
+    "url": "/static/js/2.4708429a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbaaa65d9acde380fc77",
+    "url": "/static/js/main.21bea1b0.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "5a021ab95b4d2a0c8e7511c40ca7bb2f",
+    "url": "/static/media/default-avatar.5a021ab9.png"
+  }
+]);
